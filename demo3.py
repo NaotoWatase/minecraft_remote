a5e54f25f5fdb70158cb26f16ca55305126d8f5f
Na0toWatabe <82302069+NaotoWatase@users.noreply.github.com>
2022-04-24 11:45:40 +0900

    test

--- demo3.py
+++ demo3.py
@@ -7,5 +7,4 @@ import param_MCJE as param
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat('Hello Minecraft Java Edition 1.16.5')
 
-x, y, z = mc.player.getPos()
-mc.setBlock(x+1, y, z, param.GOLD_BLOCK)
+mc.setBlock(1, 2, 2, param.GOLD_BLOCK)
