92279fa24a96be9bb68f8a7221ea0424e76569ae
Na0toWatabe <82302069+NaotoWatase@users.noreply.github.com>
2022-04-24 11:41:08 +0900

    test

--- demo3.py
+++ demo3.py
@@ -7,16 +7,5 @@ import param_MCJE as param
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat('Hello Minecraft Java Edition 1.16.5')
 
-
-data = [
-  "#  # ### #   #    ##   #   #  ##  ###  #   ### ",
-  "#  # #   #   #   #  #  # # # #  # #  # #   #  #",
-  "#### ### #   #   #  #  # # # #  # ###  #   #  #",
-  "#  # #   #   #   #  #  # # # #  # # #  #   #  #",
-  "#  # ### ### ###  ##    # #   ##  #  # ### ### "
-]
-
-for y, line in enumerate(data):
-  for x, c in enumerate(line):
-    if c == "#":
-      mc.setBlock(10, 20, 10, param.GOLD_BLOCK)
+x, y, z = mc.player.getPos()
+mc.setBlock(x+1, y, z, param.GOLD_BLOCK)
