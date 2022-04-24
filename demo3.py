b31b051b811e2e25280a95731f834aef62febaee
Na0toWatabe <82302069+NaotoWatase@users.noreply.github.com>
2022-04-24 11:37:48 +0900

    test "hello"

--- demo3.py
+++ demo3.py
@@ -3,6 +3,20 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
 
+
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat('Hello Minecraft Java Edition 1.16.5')
-mc.setBlocks(-5, 63, 5,  -5, 127, 5,  param.GOLD_BLOCK)
+
+
+data = [
+  "#  # ### #   #    ##   #   #  ##  ###  #   ### ",
+  "#  # #   #   #   #  #  # # # #  # #  # #   #  #",
+  "#### ### #   #   #  #  # # # #  # ###  #   #  #",
+  "#  # #   #   #   #  #  # # # #  # # #  #   #  #",
+  "#  # ### ### ###  ##    # #   ##  #  # ### ### "
+]
+
+for y, line in enumerate(data):
+  for x, c in enumerate(line):
+    if c == "#":
+      mc.setBlock(10, 20, 10, param.GOLD_BLOCK)
