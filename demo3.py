c78d921dd6d64e15a4c9b6b2a92a5dac44eee740
Na0toWatabe <82302069+NaotoWatase@users.noreply.github.com>
2022-06-04 18:44:55 +0900

    pyramid

--- demo3.py
+++ demo3.py
@@ -1,10 +1,33 @@
-# Hello World for Minecraft Java Edition 1.16.5
-# mcje, MCJE: Minecraft Java Edition
-from mcje.minecraft import Minecraft
-import param_MCJE as param
+# Hello World for Minecraft Pi Edition
+# mcpi, MCPI: Minecraft Pi
+from mcpi.minecraft import Minecraft
+import param_MCPI as param
+import time
 
+mc = Minecraft.create(address='192.168.1.16', port=param.PORT_MC)
+mc.postToChat('Hello Minecraft Pi Edition')
 
-mc = Minecraft.create(port=param.PORT_MC)
-mc.postToChat('Hello Minecraft Java Edition 1.16.5')
-x, y, z = mc.player.getPos()
-mc.player.setPos(x, y+100, z)
+N = 0
+M = 0
+H = 0
+S = 0
+
+x = 0
+y = 4
+z = 0
+
+block_type = 0
+
+while H < 3:
+    H = H + 1
+    while N < 11:      
+        mc.setBlocks(
+            -x, y, -z,
+            x, y, z, param.GOLD_BLOCK
+            )
+        x = x + 1
+        y = y + 1
+        z = z + 1
+        N = N + 1
+        time.sleep(0.1)
+        
