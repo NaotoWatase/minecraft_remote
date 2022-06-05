2ada0eef2d813bae441e2af4eef4d69a8384785f
Na0toWatabe <82302069+NaotoWatase@users.noreply.github.com>
2022-06-05 11:25:34 +0900

    お試し

--- demo3.py
+++ demo3.py
@@ -7,27 +7,25 @@ import time
 mc = Minecraft.create(address='192.168.1.16', port=param.PORT_MC)
 mc.postToChat('Hello Minecraft Pi Edition')
 
-N = 0
-M = 0
-H = 0
-S = 0
 
-x = 0
-y = 4
-z = 0
 
-block_type = 0
 
-while H < 3:
-    H = H + 1
-    while N < 11:      
+for block in range (3):
+    if range == 1:
+        block = param.GOLD_BLOCK
+    if range == 2:
+        block = param.ILON_BLOCK
+    if range == 3:
+        block = param.DIAMOND_BLOCK
+    for r in range (11): 
         mc.setBlocks(
             -x, y, -z,
-            x, y, z, param.GOLD_BLOCK
+            x, y, z, block
             )
         x = x + 1
         y = y + 1
         z = z + 1
         N = N + 1
-        time.sleep(0.1)
+        time.sleep(0.2)
+
         
